7a6a36a48dca12b282ddbdbe9528e83591548f7e
Vivek Kale <131688069+vivekkale9@users.noreply.github.com>
2023-12-19 13:25:23 +0530

    update

---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,33 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+
+    public class ConnectionProvider {
+        public static Connection getCon()
+        {
+            try
+            {
+
+                Class.forName("com.mysql.jdbc.Driver");
+                //Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/scms","root","karadrandi");
+
+                return con;
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+                return null;
+            }
+        }
+    }
+               
+        
+
